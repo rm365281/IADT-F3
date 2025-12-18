079233927886cb7b78a2cbbff144806d638cc8bb
Guilherme Groff <guilherme.pgb@hotmail.com>
2025-12-18 19:40:44 -0300

    Remove unnecessary comments

--- src/react_agent/graph.py
+++ src/react_agent/graph.py
@@ -15,29 +15,19 @@ from react_agent.state import InputState, State
 
 builder = StateGraph(State, input_schema=InputState, context_schema=Context)
 
-# Define the two nodes we will cycle between
 builder.add_node(clinical_react_orchestrator)
 builder.add_node(patient_info)
 builder.add_node(procedure_info)
 builder.add_node(disease_info)
-#builder.add_node(final_answer)
-#builder.add_node(ToolNode(TOOLS))
 builder.add_node(aggregator)
 builder.add_node(final_answer)
 builder.add_node(evaluator)
 
-# Set the entrypoint as `clinical_react_orchestrator`
-# This means that this node is the first one called
 builder.add_edge(START, "clinical_react_orchestrator")
 
-builder.add_conditional_edges("clinical_react_orchestrator", requires_patient_data, {True: "patient_info", False: "aggregator"})
-builder.add_conditional_edges("clinical_react_orchestrator", requires_internal_procedures, {True: "procedure_info", False: "aggregator"})
-builder.add_conditional_edges("clinical_react_orchestrator", requires_disease_info, {True: "disease_info", False: "aggregator"})
-
-# Add a conditional edge to determine the next step after `clinical_react_orchestrator`
-#builder.add_conditional_edges("clinical_react_orchestrator", tools_condition)
-
-#builder.add_edge("tools", "clinical_react_orchestrator")
+builder.add_conditional_edges("clinical_react_orchestrator", requires_patient_data, {True: "patient_info"})
+builder.add_conditional_edges("clinical_react_orchestrator", requires_internal_procedures, {True: "procedure_info"})
+builder.add_conditional_edges("clinical_react_orchestrator", requires_disease_info, {True: "disease_info"})
 
 builder.add_edge("patient_info", "aggregator")
 builder.add_edge("procedure_info", "aggregator")
@@ -47,9 +37,4 @@ builder.add_edge("final_answer", "evaluator")
 
 builder.add_conditional_edges("evaluator", route_final_answear)
 
-#builder.add_edge("sql_query", "clinical_react_orchestrator")
-#builder.add_edge("vector_search", "clinical_react_orchestrator")
-#builder.add_edge("disease_model", "clinical_react_orchestrator")
-#builder.add_edge("clinical_react_orchestrator", "final_answer")
-# Compile the builder into an executable graph
 graph = builder.compile(name="ReAct Agent")
